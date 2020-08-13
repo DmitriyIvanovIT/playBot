ccf84f51791621fd007d389a35ae95043d85f969
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-13 10:35:41 +0300

    feat: Creating a game bot normal level | /script.js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 'use strict';
 let isNumber = n => {
     return !isNaN(parseFloat(n)) && isFinite(n);
+},
+randomInteger = function (min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
 };
 
 const playBot = function () {
-    let ansverProgram = 33,
+    let ansverProgram = randomInteger(1, 100),
     userAnswer = prompt('Угадай число от 1 до 100');
 
-    if (userAnswer !== null) {
-        if (!isNumber(userAnswer)) {
-            alert('Введи число!');
-        } else if (parseFloat(userAnswer) > ansverProgram) {
-            alert('Загаданное число меньше');
-        } else if (parseFloat(userAnswer) < ansverProgram) {
-            alert('Загаданное число больше');
+    const playBotGame = function () {
+        if (userAnswer !== null) {
+            if (!isNumber(userAnswer)) {
+                alert('Введи число!');
+            } else if (parseFloat(userAnswer) > ansverProgram) {
+                alert('Загаданное число меньше');
+            } else if (parseFloat(userAnswer) < ansverProgram) {
+                alert('Загаданное число больше');
+            }
+    
+            playBotGame();
         }
-
-        playBot();
     }
+
+   
 };
 
 
