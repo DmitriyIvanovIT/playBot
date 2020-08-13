8956f605042bb087192d8eff3aa81c682a85786f
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-13 10:31:55 +0300

    feat: Creating a game bot hard level | /script.js

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 'use strict';
 let isNumber = n => {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    },
+    randomInteger = function (min, max) {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    };
 
 const playBot = function () {
-    let ansverProgram = 33,
-    userAnswer = prompt('Угадай число от 1 до 100');
+    let ansverProgram = randomInteger(1, 100),
+        numberAttempts = 10;
+    const playBotGame = () => {
+        let userAnswer = prompt('Угадай число от 1 до 100');
 
-    if (userAnswer !== null) {
-        if (!isNumber(userAnswer)) {
-            alert('Введи число!');
-        } else if (parseFloat(userAnswer) > ansverProgram) {
-            alert('Загаданное число меньше');
-        } else if (parseFloat(userAnswer) < ansverProgram) {
-            alert('Загаданное число больше');
-        }
+        if (userAnswer !== null) {
+            if (!isNumber(userAnswer)) {
+                alert('Введи число!');
+                playBotGame();
+            };
 
-        playBot();
-    }
-};
+            numberAttempts--;
+
+            if (numberAttempts !== 0) {
+                if (parseFloat(userAnswer) === ansverProgram) {
+                    userAnswer = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+
+                    if (userAnswer !== false) {
+                        playBot();
+                    }
+                } else if (parseFloat(userAnswer) > ansverProgram) {
+                    alert(`Загаданное число меньше, осталось попыток: ${numberAttempts}`);
+                    playBotGame();
+                } else if (parseFloat(userAnswer) < ansverProgram) {
+                    alert(`Загаданное число больше, осталось попыток: ${numberAttempts}`);
+                    playBotGame();
+                }
+            } else if (numberAttempts === 0) {
+            userAnswer = confirm(`Попытки закончились!! Загаданое число: ${ansverProgram}, хотите сыграть еще?`);
+
+            if (userAnswer !== false) {
+                playBot();
+            }
+        };
+        }  
+    };
+
+    playBotGame();
+}
+
+
 
 
 playBot();
